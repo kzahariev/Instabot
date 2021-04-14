601ccf77c1b4889099e5945f6146668a591e88b9
joe <kbzahariev@gmail.com>
2021-04-14 18:20:28 +0300

    v01

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from instabot import Bot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = Bot()
 
+bot.login(username="volt.bulgaria",
+          password="margarita#hristina")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+bot.upload_photo("/Downloads/",
+                 caption = "This is a post")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
