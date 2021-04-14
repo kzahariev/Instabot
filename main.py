6682d2e82feda641be5aea071baf8b339ab2933d
Krum Zahariev <68384953+kzahariev@users.noreply.github.com>
2021-04-14 18:25:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from instabot import Bot
 
 bot = Bot()
 
-bot.login(username="volt.bulgaria",
-          password="margarita#hristina")
+bot.login(username="vv",
+          password="mmh")
 
 bot.upload_photo("/Downloads/",
                  caption = "This is a post")
